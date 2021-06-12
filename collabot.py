9990c7b8808d3d9724bcccd78e0ff486f8fa68bd
정성엽 <71690205+SungyeopJeong@users.noreply.github.com>
2021-06-12 09:57:24 +0900

    Update collabot.py

--- collabot.py
+++ collabot.py
@@ -58,6 +58,7 @@ def after_stid(): # 학번 입력 후
             }
         }
         return jsonify(res)
+        print("what")
 
 @application.route('/coltype', methods=['POST'])
 def after_type(): # 유형 선택 후
